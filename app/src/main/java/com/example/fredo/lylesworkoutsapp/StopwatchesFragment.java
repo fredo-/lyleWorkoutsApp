3099991806c4d4b7e56a830bf57985d7af345a21
Alfredo Velasco <velasco622@gmail.com>
2016-11-18 14:07:33 -0500

    done setting up rxJava

--- app/src/main/java/com/example/fredo/lylesworkoutsapp/StopwatchesFragment.java
+++ app/src/main/java/com/example/fredo/lylesworkoutsapp/StopwatchesFragment.java
@@ -2,7 +2,6 @@ package com.example.fredo.lylesworkoutsapp;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -23,53 +22,24 @@ public class StopwatchesFragment extends Fragment implements View.OnClickListene
 
     private static final String TAG = "FlippingTimer";
     private int timeElapsed = 0;
-    private int interTimeElapsed = 0;
+    private int workoutTimeElapsed = 0;
     private int restTimeElapsed = 0;
+    int intervalsLeft;
     private int restDur;
-    private int intervalDur;
+    private int workoutDur;
     private int numIntervals;
     private int totalDuration;
     private boolean timersAreOn;
     private boolean inWorkout;
-    private Handler handler;
 
-    TextView intervalDisp;
+    TextView workoutDisp;
+    TextView restDisp;
     TextView totalTimerDisp;
+    TextView intervalsLeftDisp;
 
-    private int totalInt;
+    private Subscription clockSubscription;
 
-    private Subscription totalSubscription;
-    private Subscription intervalSubscription;
-
-    private Observable<Long> totalTimerObservable = Observable
-            .interval(1, TimeUnit.SECONDS)
-            .observeOn(AndroidSchedulers.mainThread());
-    private Subscriber<Long> totalSubscriber = new Subscriber<Long>() {
-                @Override
-                public void onCompleted() {
-
-                }
-
-                @Override
-                public void onError(Throwable e) {
-
-                }
-
-                @Override
-                public void onNext(Long aLong) {
-                    Log.d(TAG, "onNext: in total timer observable, timeElapsed: "+timeElapsed);
-                    if (timersAreOn) {
-                        int totalCountdown = getCountdownTimer(totalDuration, timeElapsed);
-                        totalTimerDisp.setText(getTimerDisplay(totalCountdown));
-                        timeElapsed++;
-                    }
-                    if (timeElapsed == totalDuration) {
-                        resetTimers();
-                    }
-                }
-            };
-
-    public StopwatchesFragment(){
+    public StopwatchesFragment() {
     }
 
     @Override
@@ -81,58 +51,59 @@ public class StopwatchesFragment extends Fragment implements View.OnClickListene
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_timers, container, false);
-//        runStopwatches(layout);
-        Button startBtn = (Button)layout.findViewById(R.id.startBtn);
-        Button pauseBtn = (Button)layout.findViewById(R.id.pauseBtn);
-        Button resetBtn = (Button)layout.findViewById(R.id.resetBtn);
+        Button startBtn = (Button) layout.findViewById(R.id.startBtn);
+        Button pauseBtn = (Button) layout.findViewById(R.id.pauseBtn);
+        Button resetBtn = (Button) layout.findViewById(R.id.resetBtn);
         startBtn.setOnClickListener(this);
         pauseBtn.setOnClickListener(this);
         resetBtn.setOnClickListener(this);
 
-        intervalDisp        = (TextView)layout.findViewById(R.id.intervalTimerDisplay);
-        totalTimerDisp      = (TextView)layout.findViewById(R.id.totalTimerDisplay);
+        workoutDisp         = (TextView) layout.findViewById(R.id.intervalTimerDisplay);
+        restDisp            = (TextView) layout.findViewById(R.id.restTimerDisplay);
+        totalTimerDisp      = (TextView) layout.findViewById(R.id.totalTimerDisplay);
+        intervalsLeftDisp   = (TextView) layout.findViewById(R.id.intervalsLeftDisplay);
+
+        setDefaultCountdownDisplays();
+        startClock();
+
         return layout;
     }
 
-    public void runStopwatches() {
-        View layout = getView();
-        final TextView numIntervalDisp    = (TextView)layout.findViewById(R.id.intervalsLeftDisplay);
-        final TextView restDisp           = (TextView)layout.findViewById(R.id.restTimerDisplay);
+    public void startClock() {
+        clockSubscription = Observable
+                .interval(1, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Long>() {
+                    @Override
+                    public void onCompleted() {
 
-        String initIntervalTimeToDisp   = getTimerDisplay(getCountdownTimer(intervalDur, interTimeElapsed));
-        String initRestTimeToDisp       = getTimerDisplay(getCountdownTimer(restDur, restTimeElapsed));
-        intervalDisp.setText(initIntervalTimeToDisp);
-        restDisp.setText(initRestTimeToDisp);
+                    }
 
-    }
+                    @Override
+                    public void onError(Throwable e) {
 
-    private void incrementTimesElapsed(boolean runIntervalTimer) {
-        if(runIntervalTimer) {
-            interTimeElapsed++;
-        }else {
-            restTimeElapsed++;
-        }
-        timeElapsed++;
-    }
+                    }
 
-    private boolean checkTimersProgress(int intervalTimer, int restTimer, boolean initialValue) {
-        boolean intervalRunning = initialValue;
-        if(intervalTimer==0) {
-            intervalRunning = false;
-            interTimeElapsed++;
-        }else if(restTimer==0){
-            intervalRunning = true;
-            restTimeElapsed++;
-        }
-        return intervalRunning;
-    }
-
-    private int getIntervalsLeft(int numIntervals, int timeElapsed, int restDur, int intervalDur) {
-        return numIntervals - timeElapsed/(restDur + intervalDur);
+                    @Override
+                    public void onNext(Long aLong) {
+                        Log.d(TAG, "onNext: in total timer observable, timeElapsed: " + timeElapsed);
+                        if (timersAreOn) {
+                            timeElapsed++;
+                            updateCountdownDisplays(inWorkout);
+                            inWorkout = stillInWorkoutCheck();
+                            intervalsLeftDisp.setText(String.format(Locale.US, "%d", intervalsLeft));
+                            int totalCountdown = getCountdownTimer(totalDuration, timeElapsed);
+                            totalTimerDisp.setText(getTimerDisplay(totalCountdown));
+                        }
+                        if (timeElapsed == (totalDuration+1)) {
+                            resetAllTimers();
+                        }
+                    }
+                });
     }
 
     private int getCountdownTimer(int intervalDur, int intervalTimeElapsed) {
-        return intervalDur - (intervalTimeElapsed % (intervalDur+1));
+        return intervalDur - (intervalTimeElapsed % (intervalDur + 1));
     }
 
     private String getTimerDisplay(int timer) {
@@ -147,7 +118,7 @@ public class StopwatchesFragment extends Fragment implements View.OnClickListene
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.startBtn:
                 startTimers();
                 break;
@@ -155,7 +126,7 @@ public class StopwatchesFragment extends Fragment implements View.OnClickListene
                 pauseTimers();
                 break;
             case R.id.resetBtn:
-                resetTimers();
+                resetAllTimers();
                 break;
         }
     }
@@ -163,31 +134,44 @@ public class StopwatchesFragment extends Fragment implements View.OnClickListene
     private void startTimers() {
         timersAreOn = true;
         inWorkout = true;
-        startWorkoutTimer();
-        startTotalTimer();
     }
 
     private void pauseTimers() {
         timersAreOn = false;
-        totalSubscription.unsubscribe();
-        intervalSubscription.unsubscribe();
     }
 
-    private void resetTimers() {
-        timeElapsed = 0;
-        interTimeElapsed = 0;
+    private void resetIntervalTimers() {
+        workoutTimeElapsed = 0;
         restTimeElapsed = 0;
+        workoutDisp .setText(getTimerDisplay(workoutDur));
+        restDisp    .setText(getTimerDisplay(restDur));
+    }
+
+    private void resetAllTimers() {
+        timeElapsed = 0;
+        workoutTimeElapsed = 0;
+        restTimeElapsed = 0;
+        intervalsLeft = numIntervals;
         pauseTimers();
+        setDefaultCountdownDisplays();
     }
 
     public void setWorkoutDetails(int numIntervals, int intervalDur, int restDur) {
         this.numIntervals = numIntervals;
-        this.intervalDur = intervalDur;
+        this.intervalsLeft = numIntervals;
+        this.workoutDur = intervalDur;
         this.restDur = restDur;
-        this.totalDuration= (restDur + intervalDur) * numIntervals;
-
+        this.totalDuration = (restDur + intervalDur) * numIntervals;
+        setDefaultCountdownDisplays();
     }
 
+
+    private void setDefaultCountdownDisplays() {
+        intervalsLeftDisp   .setText(String.format(Locale.US, "%d", numIntervals));
+        workoutDisp         .setText(getTimerDisplay(workoutDur));
+        restDisp            .setText(getTimerDisplay(restDur));
+        totalTimerDisp      .setText(getTimerDisplay(totalDuration));
+    }
 
     @Override
     public void onAttach(Context context) {
@@ -197,47 +181,30 @@ public class StopwatchesFragment extends Fragment implements View.OnClickListene
     @Override
     public void onDetach() {
         super.onDetach();
+        clockSubscription.unsubscribe();
     }
 
-
-    public  void startWorkoutTimer() {
-        intervalSubscription = Observable
-                .interval(1, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Long>() {
-                    @Override
-                    public void onCompleted() {
-                        //call startRest();
-                        //onCompleted is only called when you have a .take(limit) means do limit number of emissions
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(Long aLong) {
-                        if(timersAreOn){
-                            int workoutCountdown = getCountdownTimer(intervalDur, interTimeElapsed);
-                            intervalDisp.setText(getTimerDisplay(workoutCountdown));
-                            interTimeElapsed++;
-                        }
-                        if(interTimeElapsed==intervalDur) {
-                            this.unsubscribe();
-                            //startRestTimer();
-                        }
-                    }
-                });
-    }
-
-
-    public void startTotalTimer(){
-        if(totalSubscription ==  null) {
-            totalSubscription = totalTimerObservable
-                    .subscribe(totalSubscriber);
+    private void updateCountdownDisplays(boolean inWorkout) {
+        if(inWorkout) {
+            workoutTimeElapsed++;
+            int timer = getCountdownTimer(workoutDur, workoutTimeElapsed);
+            workoutDisp.setText(getTimerDisplay(timer));
         }else{
-            totalTimerObservable.retry();
+            restTimeElapsed++;
+            int timer = getCountdownTimer(restDur, restTimeElapsed);
+            restDisp.setText(getTimerDisplay(timer));
         }
+    }
+
+
+    private boolean stillInWorkoutCheck() {
+        if(restTimeElapsed == restDur || workoutTimeElapsed == workoutDur){
+            inWorkout = !inWorkout;
+            if(restTimeElapsed == restDur) {
+                intervalsLeft = intervalsLeft - 1;
+            }
+            resetIntervalTimers();
+        }
+        return inWorkout;
     }
 }
